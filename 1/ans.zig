4388529ed257763087d3d074e280db7463ffa058
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2020-12-08 12:59:32 +0800

    Add testing to zig day 1

--- 1/ans.zig
+++ 1/ans.zig
@@ -4,31 +4,31 @@ const stdout = std.io.getStdOut().writer();
 
 const MAX_INPUT_SIZE = 1024;
 
-fn readInput(allocator: *std.mem.Allocator) !std.ArrayList(i32) {
+fn readInput(allocator: *std.mem.Allocator, reader: struct {  }) !std.ArrayList(i32) {
     var buf: [MAX_INPUT_SIZE]u8 = undefined;
     var list = std.ArrayList(i32).init(allocator);
     while (true) {
-        const line = (try stdin.readUntilDelimiterOrEof(&buf, '\n')) orelse return list;
+        const line = (try reader.readUntilDelimiterOrEof(&buf, '\n')) orelse return list;
         const num = try std.fmt.parseInt(i32, line, 10);
         try list.append(num);
     }
 }
 
-fn part1(input: std.ArrayList(i32)) !void {
+fn part1(input: std.ArrayList(i32)) !i64 {
     for (input.items) |a| {
         for (input.items) |b| {
             if (a == b) {
                 continue;
             }
             if (a + b == 2020) {
-                try stdout.print("{}\n", .{a * b});
-                return;
+                return a * b;
             }
         }
     }
+    unreachable;
 }
 
-fn part2(input: std.ArrayList(i32)) !void {
+fn part2(input: std.ArrayList(i32)) !i64 {
     for (input.items) |a| {
         for (input.items) |b| {
             if (a == b) {
@@ -39,12 +39,12 @@ fn part2(input: std.ArrayList(i32)) !void {
                     continue;
                 }
                 if (a + b + c == 2020) {
-                    try stdout.print("{}\n", .{a * b * c});
-                    return;
+                    return a * b * c;
                 }
             }
         }
     }
+    unreachable;
 }
 
 pub fn main() !void {
@@ -52,9 +52,28 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = &arena.allocator;
 
-    const input = try readInput(allocator);
+    const input = try readInput(allocator, stdin);
     // Part 1
-    try part1(input);
+    const ans1 = try part1(input);
+    try stdout.print("{}\n", .{ans1});
     // Part 2
-    try part2(input);
+    const ans2 = try part2(input);
+    try stdout.print("{}\n", .{ans2});
+}
+
+const expect = @import("std").testing.expect;
+test "test input" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator;
+    const reader = std.io.Reader()
+    var input = std.ArrayList(i32).init(allocator);
+    try input.append(1721);
+    try input.append(979);
+    try input.append(366);
+    try input.append(299);
+    try input.append(675);
+    try input.append(1456);
+    expect((try part1(input)) == 514579);
+    expect((try part2(input)) == 241861950);
 }
