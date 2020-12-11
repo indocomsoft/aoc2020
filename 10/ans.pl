5e76e605292b273b280fad61d50c3ed4dc2c1708
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2020-12-11 22:08:05 +0800

    Remove CLPFD

--- 10/ans.pl
+++ 10/ans.pl
@@ -1,5 +1,3 @@
-:- use_module(library(clpfd)).
-
 input([]) :- at_end_of_stream(current_input).
 input([X|Xs]) :-
   read_line_to_string(current_input, Line),
@@ -8,17 +6,17 @@ input([X|Xs]) :-
 
 part1(Sorted, Ans) :-
   part1_helper(Sorted, 0, NumOneJolt, NumThreeJolt),
-  Ans #= NumOneJolt * NumThreeJolt.
+  Ans is NumOneJolt * NumThreeJolt.
 
 part1_helper([], _, 0, 1).
 part1_helper([X|Xs], Current, NumOneJolt, NumThreeJolt) :-
-  Diff #= X - Current,
+  Diff is X - Current,
   part1_helper(Xs, X, NewNumOneJolt, NewNumThreeJolt),
-  (Diff #= 1 ->
+  (Diff is 1 ->
     NumOneJolt is NewNumOneJolt + 1
   ; NumOneJolt is NewNumOneJolt
   ),
-  (Diff #= 3 ->
+  (Diff is 3 ->
     NumThreeJolt is NewNumThreeJolt + 1
   ; NumThreeJolt is NewNumThreeJolt
   ).
@@ -31,19 +29,19 @@ num_valid_arrangement([X|Xs], Ans) :-
   % Try for a further 2 after the immediately next one.
   % Beyond 3 from current, it is guaranteed there will be no element within 3
   % from the current jolt.
-  ([_,X2|T2] = Xs, X2 - X #=< 3 ->
+  ([_,X2|T2] = Xs, X2 - X =< 3 ->
     num_valid_arrangement([X2|T2], Ans2),
-    ([X3|T3] = T2, X3 - X #=< 3 ->
+    ([X3|T3] = T2, X3 - X =< 3 ->
       num_valid_arrangement([X3|T3], Ans3),
-      Ans #= Ans1 + Ans2 + Ans3
-    ; Ans #= Ans1 + Ans2
+      Ans is Ans1 + Ans2 + Ans3
+    ; Ans is Ans1 + Ans2
     )
-  ; Ans #= Ans1
+  ; Ans is Ans1
   ).
 
 part2(Sorted, Ans) :-
   max_list(Sorted, Max),
-  End #= Max + 3,
+  End is Max + 3,
   append([[0], Sorted, [End]], Input),
   num_valid_arrangement(Input, Ans).
 
