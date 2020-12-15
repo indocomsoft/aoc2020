c0bcd97d0c72f7047c96067bf69d4023b4b16b1b
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2020-12-15 21:47:24 +0800

    Add answer for day 15 in zig

---
+++ 15/ans.zig
@@ -0,0 +1,53 @@
+const std = @import("std");
+const stdin = std.io.getStdIn().reader();
+const stdout = std.io.getStdOut().writer();
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+
+const MAX_INPUT_SIZE = 1024;
+
+fn readInput(allocator: *Allocator) !ArrayList(u64) {
+    var buf: [MAX_INPUT_SIZE]u8 = undefined;
+    var list = std.ArrayList(u64).init(allocator);
+    const line = (try stdin.readUntilDelimiterOrEof(&buf, '\n')).?;
+    var iterator = std.mem.split(line, ",");
+    while (iterator.next()) |item| {
+        const num = try std.fmt.parseUnsigned(u64, item, 10);
+        try list.append(num);
+    }
+    return list;
+}
+
+fn run_game(input: anytype, stop: u64) !u64 {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator;
+
+    var map = std.AutoHashMap(u64, u64).init(allocator);
+    for (input.items) |item, i| {
+        try map.put(item, i + 1);
+    }
+
+    var num = input.items[input.items.len - 1];
+
+    var turn: u64 = input.items.len + 1;
+    while (turn <= stop) : (turn += 1) {
+        const newNum = if (map.contains(num)) (turn - map.get(num).? - 1) else 0;
+        try map.put(num, turn - 1);
+        num = newNum;
+    }
+
+    return num;
+}
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = &arena.allocator;
+
+    const input = try readInput(allocator);
+    const ans1 = try run_game(input, 2020);
+    try stdout.print("{}\n", .{ans1});
+    const ans2 = try run_game(input, 30000000);
+    try stdout.print("{}\n", .{ans2});
+}
